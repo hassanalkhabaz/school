a251d8d15b86497bdd37f00e3e30fee6f371cacd
sharloo12 <dashmetal.12@gmail.com>
2021-05-30 00:58:02 +0200

    Payments refactored

--- lib/ui/pages/payments.dart
+++ lib/ui/pages/payments.dart
@@ -1,3 +1,4 @@
+import 'package:flu/ui/widgets/CardView.dart';
 import 'package:flutter/material.dart';
 
 class Payments extends StatefulWidget {
@@ -6,12 +7,91 @@ class Payments extends StatefulWidget {
 }
 
 class _PaymentsState extends State<Payments> {
+  double horizontalPadding = 10;
+  TextStyle textStyle;
   @override
   Widget build(BuildContext context) {
-    return Center(
+    textStyle = TextStyle(
+      fontSize: 20,
+      color: Colors.black54,
+    );
+
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: Text('Payments'),
+          backgroundColor: Colors.cyan[400],
+          bottom: buildBottomShape(),
+        ),
+        body: Container(
+            child: ListView.builder(
+                itemCount: payData.length,
+                itemBuilder: (context, index) {
+                  return CardView(
+                    cardHeader: Text(
+                      payData[index]['date'],
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black54,
+                      ),
+                    ),
+                    cardBody: buildCardBody(
+                      payData[index]['paid'],
+                      payData[index]['remain'],
+                    ),
+                  );
+                })));
+  }
+
+  PreferredSize buildBottomShape() {
+    return PreferredSize(
+      preferredSize: Size(0, 20),
       child: Container(
-        child: Text('payments'),
+        height: 15,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.grey[100],
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(80),
+            topRight: Radius.circular(80),
+          ),
+        ),
       ),
     );
   }
+
+  Widget buildCardBody(String paid, String remain) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ListTile(
+          contentPadding: EdgeInsets.symmetric(horizontal: 10),
+          title: Text('Paid Amount', style: textStyle),
+          trailing: Text(
+            paid,
+            style: textStyle.copyWith(color: Colors.green),
+          ),
+        ),
+        ListTile(
+          contentPadding: EdgeInsets.symmetric(horizontal: 10),
+          title: Text('Remaining amount', style: textStyle),
+          trailing: Text(
+            remain,
+            style: textStyle.copyWith(color: Colors.red),
+          ),
+        ),
+      ],
+    );
+  }
+
+  //TODO: temp data for testing
+  List payData = [
+    {'date': 'May 2,2021', 'paid': '1200 S.P', 'remain': '300 S.P'},
+    {'date': 'May 2,2021', 'paid': '1200 S.P', 'remain': '300 S.P'},
+    {'date': 'May 2,2021', 'paid': '1200 S.P', 'remain': '300 S.P'},
+    {'date': 'May 2,2021', 'paid': '1200 S.P', 'remain': '300 S.P'},
+    {'date': 'May 2,2021', 'paid': '1200 S.P', 'remain': '300 S.P'},
+    {'date': 'May 2,2021', 'paid': '1200 S.P', 'remain': '300 S.P'},
+    {'date': 'May 2,2021', 'paid': '1200 S.P', 'remain': '300 S.P'},
+  ];
 }

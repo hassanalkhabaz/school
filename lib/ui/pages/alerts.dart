021ae4d97927ad4ed9a00742934e1804f00e2481
sharloo12 <dashmetal.12@gmail.com>
2021-05-29 21:21:09 +0200

    alerts refactored

--- lib/ui/pages/alerts.dart
+++ lib/ui/pages/alerts.dart
@@ -1,3 +1,4 @@
+import 'package:flu/ui/widgets/CardView.dart';
 import 'package:flutter/material.dart';
 
 class Alerts extends StatefulWidget {
@@ -8,9 +9,38 @@ class Alerts extends StatefulWidget {
 class _AlertsState extends State<Alerts> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text('alerts'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Alerts'),
+        backgroundColor: Colors.cyan[400],
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 2),
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return CardView(
+              cardHeader: Text('2021-06-01', style: TextStyle(fontSize: 16)),
+              cardBody: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 10).copyWith(top: 10),
+                    child: Text('Reason of the Alert',
+                        style: TextStyle(fontSize: 16)),
+                  ),
+                  Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 10).copyWith(top: 10),
+                    child: Text('some description about it!'),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }

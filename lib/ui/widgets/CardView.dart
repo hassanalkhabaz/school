fe981f8f17717e36c7aec9c6ad1a65a7d3ce8707
sharloo12 <dashmetal.12@gmail.com>
2021-05-30 00:36:21 +0200

    card view box radius removed

--- lib/ui/widgets/CardView.dart
+++ lib/ui/widgets/CardView.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 
 // ignore: non_constant_identifier_names
-Widget CardView(
-    {@required Widget cardHeader, @required Widget cardBody, headerColor}) {
+Widget CardView({
+  @required Widget cardHeader,
+  @required Widget cardBody,
+  headerColor,
+}) {
   return Card(
-    elevation: 2,
+    elevation: 0,
+    margin: EdgeInsets.zero,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
     child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
           width: double.infinity,
           decoration: BoxDecoration(
-            color: headerColor ?? Colors.grey[400],
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(4),
-              topRight: Radius.circular(4),
-            ),
+            color: headerColor ?? Colors.grey[100],
           ),
           padding: EdgeInsets.all(10),
           child: cardHeader,
         ),
         cardBody,
-        SizedBox(height: 10)
       ],
     ),
   );

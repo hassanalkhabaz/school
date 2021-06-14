19e78aaa745539809d2fd5fefece66e7eae59050
laolao <leenallaf98@gmail.com>
2021-06-14 16:09:52 +0300

    programme added
    Co-authored-by: sharloo12 <sharloo12@users.noreply.github.com>

--- lib/ui/pages/programs.dart
+++ lib/ui/pages/programs.dart
@@ -8,9 +8,31 @@ class Programs extends StatefulWidget {
 class _ProgramsState extends State<Programs> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text('programs'),
+    return Scaffold(
+      appBar:
+          AppBar(title: Text('Programme'), backgroundColor: Colors.purple[400]),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Programme name '),
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.white.withOpacity(.12),
+              radius: 200,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Icon(
+                  Icons.image,
+                  color: Colors.purple[100],
+                  size: 50,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
